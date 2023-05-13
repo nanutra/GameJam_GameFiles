6bab338087ea8bec0ac1534919ab429c97a0b95f
Bru Thomas <narmalone.game@gmail.com>
2023-05-13 20:01:56 +0200

    r

--- Assets/Script/Managers/FolderManager.cs
+++ Assets/Script/Managers/FolderManager.cs
@@ -8,23 +8,37 @@ public class FolderManager : MonoBehaviour
     private GameObject _gameFileFolderObject;
 
     [SerializeField]
+    private GameObject _gameWindowObject;
+
+    [SerializeField]
     private CustomEvent _onGameFileOpenEvent;
+
+    [SerializeField]
+    private CustomEvent _onGameFileCloseEvent;
 
     private void OnEnable()
     {
         _onGameFileOpenEvent.myEvent += OnGameFileOpen;
+        _onGameFileCloseEvent.myEvent += OnGameFileClosed;
     }
 
     private void OnDisable()
     {
         _onGameFileOpenEvent.myEvent -= OnGameFileOpen;
+        _onGameFileCloseEvent.myEvent -= OnGameFileClosed;
     }
 
+    /// <summary>
+    /// raised when double click on game file
+    /// </summary>
     public void OnGameFileOpen()
     {
         _gameFileFolderObject.SetActive(true);
     }
 
+    /// <summary>
+    /// raised when play click on close cross
+    /// </summary>
     public void OnGameFileClosed()
     {
         _gameFileFolderObject.SetActive(false);
